677a487dd82b5af83e74181b2a2fabb867bb8440
doubleaykay <the.a.cars@gmail.com>
2017-07-19 18:42:38 -0400

    initial commit from android studio

---
+++ app/src/main/java/mystikos/phonelookup/MainActivity.java
@@ -0,0 +1,21 @@
+package mystikos.phonelookup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Jphone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void lookup(View v) {
+
+    }
+}
